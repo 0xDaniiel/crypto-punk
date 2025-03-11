6805858df2ac17083551cc4347d3144c08774c67
Danny <dannydotdev@gmail.com>
2025-03-11 18:40:10 +0100

    improved code structure and readability

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,63 +3,31 @@ import { Button } from "./ui/button";
 import Image from "next/image";
 
 const heroImages = [
-  {
-    path: "/images/punk1.svg",
-    alt: "punk1",
-  },
-  {
-    path: "/images/punk2.svg",
-    alt: "punk2",
-  },
-  {
-    path: "/images/punk3.svg",
-    alt: "punk3",
-  },
-  {
-    path: "/images/punk4.svg",
-    alt: "punk4",
-  },
-  {
-    path: "/images/punk5.svg",
-    alt: "punk5",
-  },
-  {
-    path: "/images/punk6.svg",
-    alt: "punk6",
-  },
-  {
-    path: "/images/punk7.svg",
-    alt: "punk7",
-  },
-  {
-    path: "/images/punk8.svg",
-    alt: "punk8",
-  },
+  "/images/punk1.svg",
+  "/images/punk2.svg",
+  "/images/punk3.svg",
+  "/images/punk4.svg",
+  "/images/punk5.svg",
+  "/images/punk6.svg",
+  "/images/punk7.svg",
+  "/images/punk8.svg",
 ];
 
 const mobileImages = [
-  {
-    path: "/images/punk1.svg",
-    alt: "mobilepunk1",
-  },
-  {
-    path: "/images/punk2.svg",
-    alt: "mobilepunk2",
-  },
-  {
-    path: "/images/punk8.svg",
-    alt: "mobilepunk8",
-  },
+  "/images/punk1.svg",
+  "/images/punk2.svg",
+  "/images/punk8.svg",
 ];
+
 const Hero = () => {
   return (
     <main className="flex flex-col items-center min-h-[90vh] w-full">
       {/* Content Section */}
-      <div className="text-white flex items-center flex-col text-center gap-5 px-7 mt-20 flex-grow">
-        <h1 className="md:text-4xl text-base font-bold">
+      <div className="text-white flex flex-col items-center text-center gap-5 px-7 mt-20 flex-grow">
+        <h1 className="text-base md:text-4xl font-bold">
           The project that inspired the modern cryptoArt movement
         </h1>
-        <p className="md:text-base text-[10px] font-light">
+        <p className="text-[10px] md:text-base font-light">
           10,000 unique collectible characters with proof of ownership stored on
           the Ethereum blockchain.
         </p>
@@ -69,26 +37,26 @@ const Hero = () => {
       </div>
 
       {/* Desktop & iPad Landscape */}
-      <section className="hidden sm:flex gap-4 w-full mb-5 justify-center flex-wrap">
-        {heroImages.map((image, index) => (
+      <section className="hidden sm:flex flex-wrap justify-center gap-4 w-full mb-5">
+        {heroImages.map((src, index) => (
           <Image
             key={index}
-            src={image.path}
-            alt={image.alt}
-            width={120} // Standard size for iPads & desktops
+            src={src}
+            alt={`punk${index + 1}`}
+            width={120}
             height={120}
-            className="lg:w-[150px] lg:h-[150px] sm:w-[100px] sm:h-[100px] max-w-full h-auto"
+            className="h-auto max-w-full sm:w-[100px] sm:h-[100px] lg:w-[150px] lg:h-[150px]"
           />
         ))}
       </section>
 
       {/* Mobile Only */}
-      <section className="flex sm:hidden gap-4 w-full mb-5 justify-center">
-        {mobileImages.map((img, index) => (
+      <section className="flex sm:hidden justify-center gap-4 w-full mb-5">
+        {mobileImages.map((src, index) => (
           <Image
             key={index}
-            src={img.path}
-            alt={img.alt}
+            src={src}
+            alt={`mobilepunk${index + 1}`}
             width={80}
             height={80}
             className="w-[80px] h-[80px] max-w-full"
